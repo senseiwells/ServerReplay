2b60c1708a47d99d15bd89f1839c2842aba79abe
senseiwells <wellsm488@gmail.com>
2023-08-15 00:23:46 +0100

    Remove replay studio

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,12 @@ plugins {
     kotlin("jvm")
     id("fabric-loom")
     id("io.github.juuxel.loom-quiltflower").version("1.7.3")
-    id("com.github.johnrengelman.shadow") version "7.1.2"
     `maven-publish`
     java
 }
 
 group = property("maven_group")!!
 version = property("mod_version")!!
-
-val shade: Configuration by configurations.creating
 
 repositories {
     maven {
@@ -37,12 +34,12 @@ dependencies {
     modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
     // modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
 
-    shade(modImplementation("com.github.ReplayMod:ReplayStudio:a1e2b83") {
+    modImplementation("com.github.ReplayMod:ReplayStudio:a1e2b83") {
+        exclude(group = "org.slf4j")
         exclude(group = "com.google.guava", module = "guava-jdk5")
         exclude(group = "com.google.guava", module = "guava")
         exclude(group = "com.google.code.gson", module = "gson")
-        isTransitive = false
-    })
+    }
     include(modImplementation("net.fabricmc:fabric-language-kotlin:${property("fabric_kotlin_version")}")!!)
 
     // include(implementation(annotationProcessor("com.github.llamalad7.mixinextras:mixinextras-fabric:0.2.0-beta.6")!!)!!)
@@ -55,18 +52,6 @@ tasks {
         inputs.property("version", project.version)
         filesMatching("fabric.mod.json") {
             expand(mutableMapOf("version" to project.version))
-        }
-    }
-
-    prepareRemapJar {
-        dependsOn("shadowJar")
-    }
-
-    remapJar {
-        remapperIsolation.set(true)
-        inputFile.set(shadowJar.get().archiveFile.get())
-        doLast {
-            // shadowJar.archiveFile.get().asFile.delete()
         }
     }
 
@@ -94,20 +79,6 @@ tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "17"
     }
-}
-
-tasks.shadowJar {
-    configurations = listOf(shade)
-
-    from("LICENSE")
-
-    relocate("org.slf4j", "me.senseiwells.org.slf4j")
-    exclude("it.unimi", "com.steveice.opennbt", "org.apache")
-
-    minimize()
-    //archiveClassifier.set("fat")
-
-    // archiveFileName.set("${rootProject.name}-${archiveVersion.get()}.jar")
 }
 
 java {

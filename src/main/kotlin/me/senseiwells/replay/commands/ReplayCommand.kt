5babf16f0319e57a74387d21d78ac1137f695552
senseiwells <wellsm488@gmail.com>
2024-02-20 20:54:09 +0000

    Fix command output

--- src/main/kotlin/me/senseiwells/replay/commands/ReplayCommand.kt
+++ src/main/kotlin/me/senseiwells/replay/commands/ReplayCommand.kt
@@ -304,19 +304,17 @@ object ReplayCommand {
 
             context.source.server.execute {
                 context.source.sendSuccess(
-                    { Component.literal(builder.removeSuffix("\n").toString()) },
+                    Component.literal(builder.removeSuffix("\n").toString()),
                     true
                 )
             }
         }
 
-        context.source.sendSuccess({
-            var message = "Generating replay status..."
-            if (ServerReplay.config.includeCompressedReplaySizeInStatus) {
-                message += "\nCalculating compressed sizes of replays (this may take a while)"
-            }
-            Component.literal(message)
-        }, true)
+        var message = "Generating replay status..."
+        if (ServerReplay.config.includeCompressedReplaySizeInStatus) {
+            message += "\nCalculating compressed sizes of replays (this may take a while)"
+        }
+        context.source.sendSuccess(Component.literal(message), true)
         return 1
     }
 

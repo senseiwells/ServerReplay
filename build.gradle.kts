27991d788fcf054bc8ffc95e8be85ef2dd4ddf96
senseiwells <wellsm488@gmail.com>
2025-05-17 15:56:27 +0100

    chore: Version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ repositories {
 }
 
 
-val modVersion = "2.2.0-beta.2"
+val modVersion = "2.2.0-beta.3"
 val releaseVersion = "${modVersion}+mc${libs.versions.minecraft.get()}"
 version = releaseVersion
 group = "me.senseiwells"

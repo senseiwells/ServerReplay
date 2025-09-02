7e4f97b5aac643805c8200c35fddf5e801f64998
senseiwells <wellsm488@gmail.com>
2025-09-02 18:02:11 +0100

    chore: Update changelog

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,10 +78,47 @@ tasks {
             """
             # ServerReplay $modVersion
             
-            - No longer recording waypoint packets, fixes replay playback on the client
+            This update completely reworks the internals of the mod to allow for more flexibility
+            and hopefully will make future development of replay based moderation tools easier.
+            
+            This version is in *beta*, if you encounter any bugs please report them
+            to the issue tracker: https://github.com/senseiwells/ServerReplay/issues
+            
+            This version also comes with numerous features and bug fixes:
+            - Re-added the `max_file_size` config option
+              - The behaviour of this differs from the previous behavior
+                as this refers to the max *raw* file size before the replay
+                has been compressed, see the documentation for more information.
+            - Added the `record_hotbar` config option to allow for recording
+              the player's hotbar (flashback only)
+            - Added the `chunk_recording_strategy` config option to allow you
+              to specify chunk recorder pausing behaviour, allowing you to only
+              record (and otherwise pause) under certain conditions, the options are:
+              `"always"`, `"chunk_loaded"`, `"chunk_contains_player"`, and 
+              `"chunk_contains_non_spectator_player"`
+            - Replaced the `enable` config option with the `automatically_record`
+              option, as well as removed the command to enable/disable ServerReplay. 
+              This toggle was quite ambiguous as it still allowed you to record but 
+              just disabled the server automatically recording. The new config option
+              reflects this much better.
+            - Added the ability to record both flashback and replay-mod format
+              recordings for players at the same time
+              - Currently the only way to do this is to set the default encoding method
+                to replay-mod start a recording, then switch the encoding to flashback
+                (or vice versa) then starting another recording.
+            - Added the ability to chunk record the same chunk area with multiple recorders
+              - With the caveat that you each chunk recorder must have a unique name in order
+                to do this.
+              - As a consequence this also allows you to record both flashback and replay-mod
+                replays of the same chunk area at the same time.
+            - Added Simple Voice Chat support for flashback replays
+            - Fixed the recording quality of Simple Voice Chat
+            - Fixed time-limited recordings not accounting for server pausing
+            - Fixed an issue where an exception would sometimes occur when shutting
+              down the server
             """.trimIndent()
         )
-        type = STABLE
+        type = BETA
         modLoaders.add("fabric")
 
         displayName = "ServerReplay $modVersion for ${libs.versions.minecraft.get()}"

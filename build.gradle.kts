509d6de84c8f8caafdb09f65a217a173b0ed9a60
senseiwells <wellsm488@gmail.com>
2026-01-28 10:26:00 +0000

    fix: Pausing replay viewer in sp

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
     mavenCentral()
 }
 
-val modVersion = "3.2.5"
+val modVersion = "3.2.7"
 val releaseVersion = "${modVersion}+${libs.versions.minecraft.get()}"
 version = releaseVersion
 group = "me.senseiwells"
@@ -80,7 +80,7 @@ tasks {
         file = remapJar.get().archiveFile
         changelog.set(
             """
-            - Fixed race condition when recording with a file size or time limit
+            - Fix pausing in the replay viewer in singleplayer
             """.trimIndent()
         )
         type = STABLE
